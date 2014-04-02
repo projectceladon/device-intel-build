015d0695f30a658c3662fc9b70e195d8104c3adb
Andrew Boie <andrew.p.boie@intel.com>
2014-04-02 15:31:24 -0700

    update build tasks/scripts/releasetools to GMIN specification
    The way that Fastboot and EFI System Partition deliverables are
    stored in the EFI system partition has changed. There are now
    various zip and text files in the TFP under RADIO/ which have
    the components to assemble these images. We have some new scripts
    to create images from the target-files-package, as follows:
    
     bootloader_from_target_files: Create a VFAT EFI system partition
         image from the TFP
    
     fastboot_from_target_files: Create a fastboot boot image from
         the TFP
    
    Both of these scripts live under releasetools/ as they are critical
    for creating signed release artifacts from a TFP. All other scripts
    now live in the root of this repo.
    
    There is now a bootloader_from_zip script which is used by the build
    system to create the ESP image, without a TFP as intermediary.
    
    A new intel_common.py library contains frequently used functions
    for creating fastboot and ESP images.
    
    The generate_factory_images script has been extensively modified.
    It now provides three scripts for partial or complete provisioning
    of the device. There is now a new script included specifically for
    CI runs that does not disturb any of the installed non-AOSP components.
    A factory zip with testing keys is now populated with 'make dist'.
    
    Makefiles and supportings scripts for the old way of updating the
    EFI system partition have been removed
    
    Depends-Change-Id: Id2e1eae6e707f97e4a1ed00c91d6d7073bc21312
    Depends-Change-Id: I9f37836224b972430275932b50d96f869985dfc5
    Depends-Change-Id: I83ae9b0129a085c30fdce4d0237397cf3e38ab7a
    Change-Id: I7d4c99ba44dcc495704b58e69dbca5e09ef86b2b
    Signed-off-by: Andrew Boie <andrew.p.boie@intel.com>
    Reviewed-on: https://android.intel.com/177502

---
+++ releasetools/intel_common.py
@@ -0,0 +1,133 @@
+
+#
+# Copyright (C) 2014 The Android Open Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+
+import tempfile
+import os
+import sys
+import subprocess
+
+sys.path.append("build/tools/releasetools")
+import common
+
+def WriteFileToDest(img, dest):
+    """Write common.File to destination"""
+    fid = open(dest, 'w')
+    fid.write(img.data)
+    fid.flush()
+    fid.close()
+
+def MakeVFATFilesystem(root_zip, filename, title="ANDROIDIA", size=0):
+    """Create a VFAT filesystem image with all the files in the provided
+    root zipfile. The size of the filesystem, if not provided by the
+    caller, will be 101% the size of the containing files"""
+
+    root, root_zip = common.UnzipTemp(root_zip)
+    if size == 0:
+        for dpath, dnames, fnames in os.walk(root):
+            for f in fnames:
+                size += os.path.getsize(os.path.join(dpath, f))
+
+        # Add 1% extra space, minimum 32K
+        extra = size / 100
+        if extra < (32 * 1024):
+            extra = 32 * 1024
+        size += extra
+
+    # Round the size of the disk up to 32K so that total sectors is
+    # a multiple of sectors per track (mtools complains otherwise)
+    mod = size % (32 * 1024)
+    if mod != 0:
+        size = size + (32 * 1024) - mod
+
+    # mtools freaks out otherwise
+    if os.path.exists(filename):
+        os.unlink(filename)
+
+    cmd = ["mkdosfs", "-n", title, "-C", filename, str(size / 1024)]
+    p = common.Run(cmd)
+    p.wait()
+    assert p.returncode == 0, "mkdosfs failed"
+    for f in os.listdir(root):
+        in_p = os.path.join(root, f)
+        out_p = os.path.relpath(in_p, root)
+        PutFatFile(filename, in_p, out_p)
+
+
+def GetFastbootImage(unpack_dir, info_dict=None):
+    """Return a File object 'fastboot.img' with the Fastboot boot image.
+    It will either be fetched from BOOTABLE_IMAGES/fastboot.img or built
+    using RADIO/ufb_ramdisk.zip, RADIO/ufb_cmdline, and BOOT/kernel"""
+
+    if info_dict is None:
+        info_dict = common.OPTIONS.info_dict
+
+    prebuilt_path = os.path.join(unpack_dir, "BOOTABLE_IMAGES", "fastboot.img")
+    if (os.path.exists(prebuilt_path)):
+        print "using prebuilt fastboot.img"
+        return File.FromLocalFile(name, prebuilt_path)
+
+    print "building Fastboot image from target_files..."
+    ramdisk_img = tempfile.NamedTemporaryFile()
+    img = tempfile.NamedTemporaryFile()
+
+    ramdisk_tmp, ramdisk_zip = common.UnzipTemp(os.path.join(unpack_dir,
+            "RADIO", "ufb-ramdisk.zip"))
+
+    cmd = ["mkbootfs", ramdisk_tmp]
+    p1 = common.Run(cmd, stdout=subprocess.PIPE)
+    p2 = common.Run(["minigzip"], stdin=p1.stdout, stdout=ramdisk_img.file.fileno())
+
+    p2.wait()
+    p1.wait()
+    assert p1.returncode == 0, "mkbootfs of fastboot ramdisk failed"
+    assert p2.returncode == 0, "minigzip of fastboot ramdisk failed"
+
+    # use MKBOOTIMG from environ, or "mkbootimg" if empty or not set
+    mkbootimg = os.getenv('MKBOOTIMG') or "mkbootimg"
+
+    cmd = [mkbootimg, "--kernel", os.path.join(unpack_dir, "BOOT", "kernel")]
+    fn = os.path.join(unpack_dir, "RADIO", "ufb-cmdline")
+    if os.access(fn, os.F_OK):
+        cmd.append("--cmdline")
+        cmd.append(open(fn).read().rstrip("\n"))
+
+    args = info_dict.get("mkbootimg_args", None)
+    if args and args.strip():
+        cmd.extend(shlex.split(args))
+
+    cmd.extend(["--ramdisk", ramdisk_img.name,
+                "--output", img.name])
+
+    p = common.Run(cmd, stdout=subprocess.PIPE)
+    p.communicate()
+    assert p.returncode == 0, "mkbootimg of fastboot image failed"
+
+    img.seek(os.SEEK_SET, 0)
+    data = img.read()
+
+    ramdisk_img.close()
+    img.close()
+
+    return common.File("fastboot.img", data)
+
+def PutFatFile(fat_img, in_path, out_path):
+    cmd = ["mcopy", "-s", "-Q", "-i", fat_img, in_path,
+            "::"+out_path]
+    p = common.Run(cmd)
+    p.wait()
+    assert p.returncode == 0, "couldn't insert %s into FAT image" % (in_path,)
+
--- tasks/factory_scripts.mk
+++ tasks/factory_scripts.mk
@@ -7,24 +7,47 @@ intermediates := $(call intermediates-dir-for,PACKAGING,factory_scripts)
 FACTORY_SCRIPTS_BOOTLOADER_IMAGE := $(intermediates)/bootloader-$(TARGET_PRODUCT)-$(FILE_NAME_TAG).bin
 FACTORY_SCRIPTS_FASTBOOT_IMAGE := $(intermediates)/fastboot-$(TARGET_PRODUCT)-$(FILE_NAME_TAG).img
 
+# We could just get the fastboot.img and bootloader blobs from the $OUT
+# directory, but let's make sure these scripts don't bit-rot as they are
+# critical for release workflow.
+
 $(FACTORY_SCRIPTS_BOOTLOADER_IMAGE): $(BUILT_TARGET_FILES_PACKAGE)
-	# Create Intermediates
-	mkdir -p $(intermediates)
-	# Generate Boot Image
-	$(hide) ./device/intel/build/boot_efi_from_target_files.py --partition RADIO $(BUILT_TARGET_FILES_PACKAGE) $(FACTORY_SCRIPTS_BOOTLOADER_IMAGE)
+	mkdir -p $(dir $@)
+	$(hide) ./device/intel/build/releasetools/bootloader_from_target_files \
+		$(BUILT_TARGET_FILES_PACKAGE) $@
 
 $(FACTORY_SCRIPTS_FASTBOOT_IMAGE): $(BUILT_TARGET_FILES_PACKAGE)
-	# Create Intermediates
-	mkdir -p $(intermediates)
-	# Generate EFI Image
-	$(hide) ./device/intel/build/boot_img_from_target_files.py --partition FASTBOOT $(BUILT_TARGET_FILES_PACKAGE) $(FACTORY_SCRIPTS_FASTBOOT_IMAGE)
+	mkdir -p $(dir $@)
+	$(hide) MKBOOTIMG=$(BOARD_CUSTOM_MKBOOTIMG) \
+		./device/intel/build/releasetools/fastboot_from_target_files \
+		$(BUILT_TARGET_FILES_PACKAGE) $@
 
-$(FACTORY_SCRIPTS_PACKAGE_TARGET): $(FACTORY_SCRIPTS_BOOTLOADER_IMAGE) $(FACTORY_SCRIPTS_FASTBOOT_IMAGE) $(INTERNAL_UPDATE_PACKAGE_TARGET) $(DISTTOOLS) $(SELINUX_DEPENDS)
+# TODO: add scripts to create fastboot-usb.img from a target-files-package
+$(FACTORY_SCRIPTS_PACKAGE_TARGET): \
+		$(FACTORY_SCRIPTS_BOOTLOADER_IMAGE) \
+		$(FACTORY_SCRIPTS_FASTBOOT_IMAGE) \
+		$(INTERNAL_UPDATE_PACKAGE_TARGET) \
+		$(DISTTOOLS) $(SELINUX_DEPENDS) \
+		$(BOARD_GPT_INI) \
+		$(PRODUCT_OUT)/fastboot-usb.img \
+
 	@echo "Package: $@"
-	@echo ----- Making factory scripts ------
 	# Generate Package
-	$(hide) ./device/intel/build/releasetools/generate_factory_images.py --product $(TARGET_PRODUCT) --release $(BUILD_ID) --bootloader $(FACTORY_SCRIPTS_BOOTLOADER_IMAGE) --fastboot $(FACTORY_SCRIPTS_FASTBOOT_IMAGE) --update-archive $(INTERNAL_UPDATE_PACKAGE_TARGET) --fastboot-args '-t 192.168.42.1' --sleeptime 20 --no-checksum --output $(PRODUCT_OUT)
-	@echo ----- Made factory scripts --------
+	$(hide) ./device/intel/build/generate_factory_images \
+		--product $(TARGET_PRODUCT) --release $(BUILD_ID) \
+		--bootloader $(FACTORY_SCRIPTS_BOOTLOADER_IMAGE) \
+		--fastboot $(FACTORY_SCRIPTS_FASTBOOT_IMAGE) \
+		--update-archive $(INTERNAL_UPDATE_PACKAGE_TARGET) \
+		--gpt $(BOARD_GPT_INI) \
+		--unlock --erase \
+		--fastboot-args '-t 192.168.42.1' --sleeptime 30 \
+		--input $(PRODUCT_OUT)/fastboot-usb.img \
+		--no-checksum --output $(PRODUCT_OUT)
 
 .PHONY: factoryscripts
 factoryscripts: $(FACTORY_SCRIPTS_PACKAGE_TARGET)
+
+ifeq ($(TARGET_BUILD_INTEL_FACTORY_SCRIPTS),true)
+$(call dist-for-goals,droidcore,$(FACTORY_SCRIPTS_PACKAGE_TARGET))
+endif
+
