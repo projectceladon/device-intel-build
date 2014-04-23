367d967a56ed38eeeca6be3dcabc245d1e5bd860
Yong Yao <yong.yao@intel.com>
2014-04-23 16:20:26 -0700

    REVERTME: set several env variables for buildbot
    Change-Id: I149ce5664604b4dc21fb7b4cf1603d32c9a2890f
    Signed-off-by: Yong Yao <yong.yao@intel.com>
    Reviewed-on: 184218
    Reviewed-by: cactus <cactus@intel.com>

---
+++ vendorsetup.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+export PLATFORM_VERSION=OM
+echo JAVA_HOME = $JAVA_HOME
+echo "set JAVA_HOME:/usr/lib/jvm/java-7-openjdk-amd64"
+export JAVA_HOME=/usr/lib/jvm/java-7-openjdk-amd64
+echo JAVA_HOME = $JAVA_HOME
