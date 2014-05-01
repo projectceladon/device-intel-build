072cc6379348bce7ec0c35d12121095048b2a2d5
Andrew Boie <andrew.p.boie@intel.com>
2014-05-01 14:57:43 -0700

    generate_factory_images adjustments
     - Filenames inside the archive are now 'stable' and do
       not depend on what the source files are named. The
       --input parameter now takes a <src>=<dest> pair
    
     - The "fastboot reboot-bootloader" in between flashing
       the base stuff and "fastboot update" is removed as it
       isn't necessary
    
     - There are now two 'fastboot boot' commands in flash-ci.sh.
       The first enters the current Fastboot image from whatever
       old version happens to be on the DUT. The second verifies
       that 'fastboot boot' still works in the new image.
    
     - The output file is now specified on the command line,
       which plays much nicer with how GNU Make works.
       The --no-checksum option removed as it isn't relevant
       any more.
    
    Change-Id: Iaa42b2022c354a6ef4d7724f6d353eaa9e01e7bc
    Signed-off-by: Andrew Boie <andrew.p.boie@intel.com>
    Reviewed-on: 186454
    Reviewed-by: cactus <cactus@intel.com>
    Reviewed-by: Gumbel, Matthew K <matthew.k.gumbel@intel.com>

--- tasks/factory_scripts.mk
+++ tasks/factory_scripts.mk
@@ -1,6 +1,7 @@
 # -----------------------------------------------------------------
 # Factory Scripts
 
+# FIXME why the conversion to lowercase??
 BUILD_ID_LC := $(shell echo $(BUILD_ID) | tr A-Z a-z)
 FACTORY_SCRIPTS_PACKAGE_TARGET := $(PRODUCT_OUT)/$(TARGET_PRODUCT)-$(BUILD_ID_LC)-factory.tgz
 intermediates := $(call intermediates-dir-for,PACKAGING,factory_scripts)
@@ -39,6 +40,7 @@ $(FACTORY_SCRIPTS_FASTBOOT_USB): \
 		--verbose $(BUILT_TARGET_FILES_PACKAGE) $@
 
 $(FACTORY_SCRIPTS_PACKAGE_TARGET): \
+                device/intel/build/generate_factory_images \
 		$(FACTORY_SCRIPTS_BOOTLOADER_IMAGE) \
 		$(FACTORY_SCRIPTS_FASTBOOT_IMAGE) \
                 $(FACTORY_SCRIPTS_FASTBOOT_USB) \
@@ -56,8 +58,8 @@ $(FACTORY_SCRIPTS_PACKAGE_TARGET): \
 		--gpt $(BOARD_GPT_INI) \
 		--unlock --erase \
 		--sleeptime 45 \
-		--input $(FACTORY_SCRIPTS_FASTBOOT_USB) \
-		--no-checksum --output $(PRODUCT_OUT)
+		--input $(FACTORY_SCRIPTS_FASTBOOT_USB)=fastboot-usb.img \
+		--output $@
 
 .PHONY: factoryscripts
 factoryscripts: $(FACTORY_SCRIPTS_PACKAGE_TARGET)
