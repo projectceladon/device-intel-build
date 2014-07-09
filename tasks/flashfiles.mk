7f65a36300726e2ccffbd27e2d16c10de11893e3
Andrew Boie <andrew.p.boie@intel.com>
2014-07-08 17:55:24 -0700

    add rules for PFT flashfiles
    In attempt to make these rules scale across at least some platforms,
    the specific XML files included will be specified in the product
    configuration as BOARD_FLASHFILES_XML.
    
    The 'fastboot update' zip is more or less invariant and
    automatically included. Additional files needed for a product
    (like loaders, firmware blobs) are specified in BOARD_FLASHFILES
    as a <src>:<dst> pair separated by a colon.
    
    Change-Id: I826b6ad08df8ac1f782157312b7c7880b4adc49a
    Signed-off-by: Andrew Boie <andrew.p.boie@intel.com>
    Reviewed-on: https://android.intel.com/215749
    Reviewed-by: cactus <cactus@intel.com>
    Tested-by: cactus <cactus@intel.com>
    Reviewed-by: Gumbel, Matthew K <matthew.k.gumbel@intel.com>
    Reviewed-by: Phillips, Scott D <scott.d.phillips@intel.com>

---
+++ tasks/flashfiles.mk
@@ -0,0 +1,31 @@
+ifeq ($(TARGET_BUILD_INTEL_FACTORY_FLASHFILES),true)
+
+flashfiles_zip := $(OUT)/flashfiles.zip
+ff_intermediates := $(call intermediates-dir-for,PACKAGING,flashfiles)
+
+define copy-flashfile
+$(hide) $(ACP) $(1) $(2)
+
+endef
+
+$(flashfiles_zip): \
+		$(INTERNAL_UPDATE_PACKAGE_TARGET) \
+		$(foreach pair,$(BOARD_FLASHFILES),$(call word-colon,1,$(pair))) \
+		$(BOARD_FLASHFILES_XML) | $(ACP) \
+
+	$(hide) mkdir -p $(dir $@)
+	$(hide) rm -f $@
+	$(hide) rm -rf $(ff_intermediates)
+	$(hide) mkdir -p $(ff_intermediates)
+	$(hide) $(ACP) -f $(INTERNAL_UPDATE_PACKAGE_TARGET) $(ff_intermediates)/update.zip
+	$(hide) $(ACP) -f $(BOARD_FLASHFILES_XML) $(ff_intermediates)
+	$(foreach pair,$(BOARD_FLASHFILES), \
+		$(call copy-flashfile,$(call word-colon,1,$(pair)),$(ff_intermediates)/$(call word-colon,2,$(pair))))
+	$(hide) zip -qj $@ $(ff_intermediates)/*
+
+
+$(call dist-for-goals,droidcore,$(flashfiles_zip):$(TARGET_PRODUCT)-flashfiles-$(FILE_NAME_TAG).zip)
+.PHONY: flashfiles
+flashfiles: $(flashfiles_zip)
+
+endif
