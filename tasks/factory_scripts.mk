ca00199db7a3eb41fe48ced4118e2f2b1b571748
Andrew Boie <andrew.p.boie@intel.com>
2014-08-27 13:15:30 -0700

    reduce scope of factory archives
    They now just flash a bootloader and run fastboot -w update,
    plus an optional radio image. This is just like the factory images
    at https://developers.google.com/android/nexus/images
    
    Change-Id: I59d7565ef642db8819b2cf9260c69f0ce0646aa3
    Signed-off-by: Andrew Boie <andrew.p.boie@intel.com>
    Reviewed-on: https://android.intel.com/234717
    Reviewed-by: cactus <cactus@intel.com>

--- tasks/factory_scripts.mk
+++ tasks/factory_scripts.mk
@@ -4,43 +4,23 @@
 # FIXME why the conversion to lowercase??
 BUILD_ID_LC := $(shell echo $(BUILD_ID) | tr A-Z a-z)
 FACTORY_SCRIPTS_PACKAGE_TARGET := $(PRODUCT_OUT)/$(TARGET_PRODUCT)-$(BUILD_ID_LC)-factory.tgz
-FACTORY_SCRIPTS_OEMVARS := $(if $(BOARD_OEM_VARS_FILE),--oemvars $(BOARD_OEM_VARS_FILE))
 FACTORY_SCRIPTS_RADIO := $(if $(BOARD_RADIOIMAGE), --radio $(BOARD_RADIOIMAGE))
-
-# For legacy boot, we need to include the MBR boot block
-ifeq ($(TARGET_USE_SYSLINUX),true)
-	factory_scripts_mbr_boot_block = $(BOARD_MBR_BLOCK_BIN)
-	factory_scripts_mbr_boot_block_option = --mbr $(BOARD_MBR_BLOCK_BIN)
-else
-	factory_scripts_mbr_boot_block :=
-	factory_scripts_mbr_boot_block_option :=
-endif
-
 
 $(FACTORY_SCRIPTS_PACKAGE_TARGET): \
 		device/intel/build/generate_factory_images \
 		$(PRODUCT_OUT)/bootloader \
-		$(PRODUCT_OUT)/fastboot.img \
-		$(PRODUCT_OUT)/fastboot-usb.img \
 		$(INTERNAL_UPDATE_PACKAGE_TARGET) \
+		$(BOARD_RADIOIMAGE) \
 		$(DISTTOOLS) $(SELINUX_DEPENDS) \
-		$(BOARD_GPT_INI) \
-		$(factory_scripts_mbr_boot_block) \
 
 	@echo "Package: $@"
 	# Generate Package
 	$(hide) ./device/intel/build/generate_factory_images \
 		--product $(TARGET_PRODUCT) --release $(BUILD_ID) \
 		--bootloader $(PRODUCT_OUT)/bootloader \
-		--fastboot $(PRODUCT_OUT)/fastboot.img \
 		--update-archive $(INTERNAL_UPDATE_PACKAGE_TARGET) \
 		$(FACTORY_SCRIPTS_RADIO) \
-		$(FACTORY_SCRIPTS_OEMVARS) \
-		--gpt $(BOARD_GPT_INI) \
-		$(factory_scripts_mbr_boot_block_option) \
-		--unlock --erase \
 		--sleeptime 45 \
-		--input $(PRODUCT_OUT)/fastboot-usb.img=fastboot-usb.img \
 		--output $@
 
 .PHONY: factoryscripts
