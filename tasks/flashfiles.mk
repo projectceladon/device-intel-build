3a20994a82609e17b69059fcb5d020b9abc11fbc
Andrew Boie <andrew.p.boie@intel.com>
2014-09-18 15:32:07 -0700

    flashfiles_from_target_files: new way to create flashfiles
    Per Google it's not correct to provision devices using $OUT/system.img
    as incremental updates will fail. We also need a method to create
    production-signed flashfiles from a target-files-package.
    
    The existing flashfiles creation mechanism has been refactored:
    - flashfiles_from_target_files script created; accepts a TFP as
      a parameter and emits flashfiles.zip
    - flashxml.py is now a library and isn't independently executable
    - flashfiles created by 'make dist' or 'make flashfiles' now uses
      flashfiles_from_target_files to create them.
    - flashdep.py and flashtarget.py no longer used, deleted
    
    The semantics of the input JSON file has changed:
    - The "target" field for a command is now a literal filename, we
      no longer try to sniff this kind of stuff out of the system
      environment. We need to be able to create flashfiles outside
      of the Android build system.
    - The JSON "source" field is new and specifies how to get at the
      source binary when assembling the flashfiles zip:
      * fastboot - build a Fastboot boot image
      * boot:xxxx - build an AOSP boot image xxxx (either "boot" or "recovery")
      * provdatazip: Pull a named file out of RADIO/provdata.zip
      * images - pull a named file out of tfp IMAGES/
      * bootloader - Build a bootloader image
      * bootloaderzip - Pull a named file out of RADIO/bootloader.zip
    - At build time, we revert back to using BOARD_FLASHFILES, which is
      a list of colon-separated filename pairs. The first item in the
      pair is the actual path to the file in the build system and can
      be anything. The second item is the name of the file inside
      the TFP's RADIO/flashfiles.zip, and must correspond to the "target"
      field in the JSON configuration. See the changes to
      device/intel/mixins, in particular the new AndroidBoard.mk
    
    Issue: GMINL-1069, GMINL-1239
    Change-Id: Ic7f1f6d9c8eef18fa9790dc2b58f14f4709d1c12
    Signed-off-by: Andrew Boie <andrew.p.boie@intel.com>
    Reviewed-on: https://android.intel.com/274439

--- tasks/flashfiles.mk
+++ tasks/flashfiles.mk
@@ -1,27 +1,4 @@
-PUBLISH_SCRIPTS := device/intel/build
-
-FLASHFILES_CONFIG ?= $(TARGET_DEVICE_DIR)/flashfiles.json
-
-ifneq ($(wildcard $(FLASHFILES_CONFIG)),)
-
-$(eval FLASHFILES_T2F := $(shell $(PUBLISH_SCRIPTS)/flashdep.py $(FLASHFILES_CONFIG)))
-
-FLASHFILES_IMAGES := $(foreach dep,$(FLASHFILES_T2F),$(word 2,$(subst :, ,$(dep))))
-
-FLASHFILES_XML := $(shell $(PUBLISH_SCRIPTS)/flashtarget.py $(FLASHFILES_CONFIG))
-FLASHFILES_XML := $(addprefix $(PRODUCT_OUT)/,$(FLASHFILES_XML))
-FIRST_FLASHFILES_XML := $(firstword $(FLASHFILES_XML))
-OTHER_FLASHFILES_XML := $(wordlist 2,99,$(FLASHFILES_XML))
-
-# Makefile cannot handle 1 command with multiple output, so use the first one
-$(FIRST_FLASHFILES_XML): $(FLASHFILES_CONFIG) $(FLASHFILES_IMAGES)
-	$(hide) ./$(PUBLISH_SCRIPTS)/flashxml.py $< -p $(TARGET_PRODUCT) -d $(@D) -t "$(FLASHFILES_T2F)"
-
-# Add dependency for other xmls
-$(OTHER_FLASHFILES_XML): $(FIRST_FLASHFILES_XML)
-
-.PHONY: flashfiles_nozip
-flashfiles_nozip: $(FLASHFILES_XML) $(FLASHFILES_IMAGES)
+ifeq ($(USE_INTEL_FLASHFILES),true)
 
 name := $(TARGET_PRODUCT)
 ifeq ($(TARGET_BUILD_TYPE),debug)
@@ -30,12 +7,11 @@ endif
 name := $(name)-flashfiles-$(FILE_NAME_TAG)
 
 INTEL_FACTORY_FLASHFILES_TARGET := $(PRODUCT_OUT)/$(name).zip
+fftf := device/intel/build/releasetools/flashfiles_from_target_files
 
-$(INTEL_FACTORY_FLASHFILES_TARGET): $(FLASHFILES_IMAGES) $(FLASHFILES_XML)
-	$(hide) mkdir -p $(@D)
-	$(hide) rm -f $@
-	@echo generating $@
-	$(hide) zip -1 -j $@ $^
+$(INTEL_FACTORY_FLASHFILES_TARGET): $(BUILT_TARGET_FILES_PACKAGE) $(fftf)
+	$(hide) mkdir -p $(dir $@)
+	$(hide) $(fftf) $(BUILT_TARGET_FILES_PACKAGE) $@
 
 $(call dist-for-goals,droidcore,$(INTEL_FACTORY_FLASHFILES_TARGET))
 
