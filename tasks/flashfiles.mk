cc382e1f3630e8cf820f3df560717f7b5c35dc91
Anas Nashif <anas.nashif@intel.com>
2014-09-23 15:47:00 -0400

    Adapt for multiple flashfiles and signle binary image
    Change-Id: I1f22c0f7ab163ab19f929ba74d94104668f75209
    Signed-off-by: Anas Nashif <anas.nashif@intel.com>
    Reviewed-on: https://android.intel.com:443/321525

--- tasks/flashfiles.mk
+++ tasks/flashfiles.mk
@@ -5,15 +5,33 @@ ifeq ($(TARGET_BUILD_TYPE),debug)
   name := $(name)_debug
 endif
 name := $(name)-flashfiles-$(FILE_NAME_TAG)
-
 INTEL_FACTORY_FLASHFILES_TARGET := $(PRODUCT_OUT)/$(name).zip
+
+ifneq ($(FLASHFILE_VARIANTS),)
+INTEL_FACTORY_FLASHFILES_TARGET :=
+$(foreach var,$(FLASHFILE_VARIANTS), \
+	$(info Adding $(var)) \
+	$(eval var_zip := $(OUT)/$(TARGET_PRODUCT)-flashfiles-$(var)-$(FILE_NAME_TAG).zip) \
+	$(eval INTEL_FACTORY_FLASHFILES_TARGET += $(var_zip)) \
+	$(call dist-for-goals,droidcore,$(var_zip):$(TARGET_PRODUCT)-flashfiles-$(var)-$(FILE_NAME_TAG).zip))
+else
+$(call dist-for-goals,droidcore,$(INTEL_FACTORY_FLASHFILES_TARGET))
+endif
+
 fftf := device/intel/build/releasetools/flashfiles_from_target_files
 
+ifneq ($(FLASHFILE_VARIANTS),)
+$(INTEL_FACTORY_FLASHFILES_TARGET): $(BUILT_TARGET_FILES_PACKAGE) $(fftf) $(MKDOSFS) $(MCOPY)
+	$(hide) mkdir -p $(dir $@)
+	$(eval y = $(subst -, ,$(basename $(@F))))
+	$(eval DEV = $(word 3, $(y)))
+	$(hide) $(fftf) --file-path=$(OUT) --device=$(DEV) $(BUILT_TARGET_FILES_PACKAGE) $@
+else
 $(INTEL_FACTORY_FLASHFILES_TARGET): $(BUILT_TARGET_FILES_PACKAGE) $(fftf) $(MKDOSFS) $(MCOPY)
 	$(hide) mkdir -p $(dir $@)
 	$(hide) $(fftf) $(BUILT_TARGET_FILES_PACKAGE) $@
+endif
 
-$(call dist-for-goals,droidcore,$(INTEL_FACTORY_FLASHFILES_TARGET))
 
 .PHONY: flashfiles
 flashfiles: $(INTEL_FACTORY_FLASHFILES_TARGET)
