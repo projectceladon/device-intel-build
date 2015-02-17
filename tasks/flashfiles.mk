309647194374f351a723532d12ce9a530febb8dc
Andrew Boie <andrew.p.boie@intel.com>
2015-02-17 13:13:00 -0800

    flashfile.mk: create variant-specific OTA packages for IRDA
    The OTA package created by the build system is 'fixed-up' using
    ota_deployment_fixup script, creating new OTA packages tuned for
    the set of defined IRDA variants.
    
    Issue: GMINL-5809
    Change-Id: Idca3311bdc745bf272f6757c17a4fbe769c1b1b2
    Signed-off-by: Andrew Boie <andrew.p.boie@intel.com>
    Reviewed-on: https://android.intel.com/333347

--- tasks/flashfiles.mk
+++ tasks/flashfiles.mk
@@ -5,33 +5,47 @@ ifeq ($(TARGET_BUILD_TYPE),debug)
   name := $(name)_debug
 endif
 name := $(name)-flashfiles-$(FILE_NAME_TAG)
-INTEL_FACTORY_FLASHFILES_TARGET := $(PRODUCT_OUT)/$(name).zip
+fftf := device/intel/build/releasetools/flashfiles_from_target_files
+odf := device/intel/build/releasetools/ota_deployment_fixup
 
 ifneq ($(FLASHFILE_VARIANTS),)
 INTEL_FACTORY_FLASHFILES_TARGET :=
 $(foreach var,$(FLASHFILE_VARIANTS), \
 	$(info Adding $(var)) \
-	$(eval var_zip := $(OUT)/$(TARGET_PRODUCT)-flashfiles-$(var)-$(FILE_NAME_TAG).zip) \
-	$(eval INTEL_FACTORY_FLASHFILES_TARGET += $(var_zip)) \
-	$(call dist-for-goals,droidcore,$(var_zip):$(TARGET_PRODUCT)-flashfiles-$(var)-$(FILE_NAME_TAG).zip))
-else
-$(call dist-for-goals,droidcore,$(INTEL_FACTORY_FLASHFILES_TARGET))
-endif
+	$(eval fn_prefix := $(OUT)/$(TARGET_PRODUCT)) \
+	$(eval fn_suffix := $(var)-$(FILE_NAME_TAG)) \
+	$(eval ff_zip := $(fn_prefix)-flashfiles-$(fn_suffix).zip) \
+	$(eval ota_zip := $(fn_prefix)-ota-$(fn_suffix).zip) \
+	$(eval INTEL_FACTORY_FLASHFILES_TARGET += $(ff_zip)) \
+	$(eval INTEL_OTA_PACKAGES += $(ota_zip)) \
+	$(call dist-for-goals,droidcore,$(ff_zip):$(notdir $(ff_zip))) \
+	$(call dist-for-goals,droidcore,$(ota_zip):$(notdir $(ota_zip))))
 
-fftf := device/intel/build/releasetools/flashfiles_from_target_files
-
-ifneq ($(FLASHFILE_VARIANTS),)
 $(INTEL_FACTORY_FLASHFILES_TARGET): $(BUILT_TARGET_FILES_PACKAGE) $(fftf) $(MKDOSFS) $(MCOPY)
 	$(hide) mkdir -p $(dir $@)
 	$(eval y = $(subst -, ,$(basename $(@F))))
 	$(eval DEV = $(word 3, $(y)))
 	$(hide) $(fftf) --variant=$(DEV) $(BUILT_TARGET_FILES_PACKAGE) $@
+
+$(INTEL_OTA_PACKAGES): $(INTERNAL_OTA_PACKAGE_TARGET) $(BUILT_TARGET_FILES_PACKAGE) $(odf) $(DISTTOOLS)
+	$(hide) mkdir -p $(dir $@)
+	$(eval y = $(subst -, ,$(basename $(@F))))
+	$(eval DEV = $(word 3, $(y)))
+	$(hide) $(odf) --verbose --variant=$(DEV) \
+		--target_files $(BUILT_TARGET_FILES_PACKAGE) \
+		$(INTERNAL_OTA_PACKAGE_TARGET) $@
+
+otapackage: $(INTEL_OTA_PACKAGES)
+
 else
+INTEL_FACTORY_FLASHFILES_TARGET := $(PRODUCT_OUT)/$(name).zip
+
 $(INTEL_FACTORY_FLASHFILES_TARGET): $(BUILT_TARGET_FILES_PACKAGE) $(fftf) $(MKDOSFS) $(MCOPY)
 	$(hide) mkdir -p $(dir $@)
 	$(hide) $(fftf) $(BUILT_TARGET_FILES_PACKAGE) $@
-endif
 
+$(call dist-for-goals,droidcore,$(INTEL_FACTORY_FLASHFILES_TARGET))
+endif
 
 .PHONY: flashfiles
 flashfiles: $(INTEL_FACTORY_FLASHFILES_TARGET)
