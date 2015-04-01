b33bd4536643d4ae1e44c6affcdbebf5c8ffc44d
Sridhar Uyyala <sridhar.uyyala@intel.com>
2015-04-01 09:27:47 -0700

    helper scripts to store blobs in 2ndstage boot
    Issue: GMINL-5555
    Change-Id: Ie26d8de575da3c999d93d472810a8b877b10a9c8
    Signed-off-by: Sridhar Uyyala <sridhar.uyyala@intel.com>
    Reviewed-on: 349561
    Reviewed-by: Boie, Andrew P <andrew.p.boie@intel.com>
    Tested-by: Boie, Andrew P <andrew.p.boie@intel.com>

--- tasks/2ndbootloader.mk
+++ tasks/2ndbootloader.mk
@@ -1,4 +1,12 @@
+ifndef BOARD_BLOBSTORE_CONFIG
 LOCAL_DTB_PATH := $(LOCAL_KERNEL_PATH)/$(BOARD_DTB_FILE)
+else
+LOCAL_DTB_PATH := $(PRODUCT_OUT)/blobstore.bin
+$(LOCAL_DTB_PATH):
+	device/intel/build/build_blobstore.py \
+			--config $(BOARD_BLOBSTORE_CONFIG) \
+			--output $(LOCAL_DTB_PATH)
+endif
 
 $(INSTALLED_2NDBOOTLOADER_TARGET): $(LOCAL_DTB_PATH) | $(ACP)
 	$(hide) $(ACP) -fp $(LOCAL_DTB_PATH) $@
