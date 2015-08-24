e210e501d61b5fd1344035c34a9c1ec5040bc36f
Gabriel Touzeau <gabrielx.touzeau@intel.com>
2015-08-24 16:23:48 +0200

    sofia build: Phone Flash Tool fastboot flash support
    fastboot configuration is built to extract fastboot flash files from FLS
    files, and flash them using fastboot.
    
    For each FLS file present in the flash file json description and that
    has the key fasboot set to yes, two PFT commands are being added:
     - one to extract the fastboot binary from the FLS file to a temporary
       directory
     - one to flash the extracted binary with fasboot
    Default command parameters are defined through a global.
    
    On the opposite side, the timeout value for configurations other than
    fastboot are now retrieved from configurations parameters instead of
    commands and the function to add a configuration adds automatically
    all the parameters.
    
    Finally, this patch fixes the PFT "restrict" key that should be given
    a table of config name strings rather than a simple string (
    nevertheless both are supported, but the table is the official format
    for "restrict").
    
    Change-Id: Idf0d8b7b02cbf7d3b0d8c3a8989ea7033607c568
    Tracked-On: OAM-5957
    Signed-off-by: Gabriel Touzeau <gabrielx.touzeau@intel.com>
    Signed-off-by: Gaelle Nassiet <gaellex.nassiet@intel.com>
    Reviewed-on: 424610

--- releasetools/flashflsxml.py
+++ releasetools/flashflsxml.py
@@ -18,27 +18,28 @@ class FlashFileJson:
         self.prop = {}
         self.mv_args = {}
         self.mv_config_default = mv_config_default
+        self.extracts = {'psi': '.fls_ID0_PSI_LoadMap0.bin',
+                         'slb': '.fls_ID0_SLB_LoadMap0.bin',
+                         'hypervisor': '.fls_ID0_CODE_LoadMap0.bin'}
+        self.default_params = {"timeout":60000, "retry":2, "mandatory":True}
 
-    def add_file(self, shortname, target, source):
+    def add_file(self, shortname, target, source, file_type):
         filename = os.path.basename(target)
         if filename in self.flist:
             return
         if "mvconfig_" in filename and re.split("mvconfig_|_signed|\.fls", filename)[1] != self.mv_config_default:
             target = "mvconfigs/" + filename
-        self.flist[target] = source
-        new = {'type': 'file', 'name': shortname, 'value': filename, 'description': filename}
+        if file_type == "file":
+            self.flist[target] = source
+        new = {'type': file_type, 'name': shortname, 'value': filename, 'description': filename}
         self.flash['parameters'][shortname] = new
 
-    def add_configuration(self, config_name, brief, description, groupsState, name, parameters, startState, default):
-        new = {config_name: {'brief': brief,
-                            'description': description,
-                            'groupsState': groupsState,
-                            'name': name,
-                            'parameters': parameters,
-                            'startState': startState,
-                            'default': default
-                            }
-              }
+    def add_configuration(self, config, default):
+        new = {config['config_name']: {}}
+        for key in config:
+            if key != 'config_name':
+                new[config['config_name']][key] = config[key]
+        new[config['config_name']]['default'] = default
         self.flash['configurations'].update(new)
 
 
@@ -49,14 +50,14 @@ class FlashFileJson:
                'timeout': timeout,
                'retry': retry,
                'mandatory': mandatory,
-               'restrict': restrict}
+               'restrict': [restrict]}
         self.flash['commands'].append(new)
 
     def add_popup(self, ptype, description, restrict):
         new = {'tool': 'popup',
                'type': ptype,
                'description': description,
-               'restrict': restrict}
+               'restrict': [restrict]}
         self.flash['commands'].append(new)
 
     def add_buildproperties(self, buildprop):
@@ -73,7 +74,7 @@ class FlashFileJson:
             if cmd['type'] in ['fls']:
                 fname = os.path.basename(cmd['target'])
                 shortname = fname.split('.')[0].lower()
-                self.add_file(shortname, cmd['target'], cmd['source'])
+                self.add_file(shortname, cmd['target'], cmd['source'], 'file')
                 cmd['pftname'] = '${' + shortname + '}'
 
                 if 'core' in cmd:
@@ -83,15 +84,29 @@ class FlashFileJson:
                     for mv in self.mv_args:
                         self.mv_args[mv] += cmd['pftname'] + ' '
 
+                if 'fastboot' in cmd and cmd['fastboot'] == "yes":
+                    extracted_suff = self.extracts.get(cmd['partition'], '.fls_ID0_CUST_LoadMap0.bin')
+                    fname_temp = re.split("_signed|\.fls", fname)[0] + extracted_suff
+                    shortname_temp = shortname + '_temp'
+                    self.add_file(shortname_temp, fname_temp, cmd['source'], 'temporary_file')
+
+                    params = (cmd.get('timeout', self.default_params['timeout']), cmd.get('retry', self.default_params['retry']), cmd.get('mandatory', self.default_params['mandatory']))
+                    self.add_command('flsTool', ' -x ${' + shortname + '} --replace -o ${' + shortname_temp + '}', 'Extract image parts from ' + fname, 'fastboot_config', params)
+                    self.add_command('fastboot', ' flash ' +  cmd['partition'] + ' ${' + shortname_temp + '}/'  + fname_temp, 'Flashing ' +  cmd['partition'] + ' with fastboot', 'fastboot_config', params)
+
             if cmd['type'] == 'prop':
                 self.add_buildproperties(cmd['target'])
                 continue
 
         tools = 'flsDownloader'
-        params = (cmd.get('timeout', 60000), cmd.get('retry', 2), cmd.get('mandatory', True))
         for config_name in self.flash['configurations']:
             c = self.flash['configurations'][config_name]
+            if config_name == "fastboot_config":
+                self.add_command('fastboot', ' oem lock', 'lock the device', config_name, (self.default_params['timeout'], self.default_params['retry'], self.default_params['mandatory']))
+                self.add_command('fastboot', ' continue ', 'fastboot continue', config_name, (self.default_params['timeout'], self.default_params['retry'], self.default_params['mandatory']))
+                continue
             mv_desc = "Flashing " + c['name'] + " image"
+            params = (c.get('timeout'), c.get('retry', self.default_params['retry']), c.get('mandatory', self.default_params['mandatory']))
             if 'erase' in config_name:
                 self.mv_args[config_name] = '--erase-mode=1 ' + self.mv_args[config_name]
                 self.add_popup('warn', "This configuration will erase *all* your NVM, including calibration data. Unplug now your board to abort", config_name)
@@ -100,13 +115,15 @@ class FlashFileJson:
 
     def parse_configuration(self, configurations):
         for config in configurations:
-            self.mv_args[config['config_name']] = ""
-            if config['config_name'].split("_fls_config")[0] == self.mv_config_default:
-                default = True
+            default = False
+            if config['config_name'] == "fastboot_config":
+                self.add_command('fastboot', ' oem unlock', 'unlock the device', config['config_name'], (self.default_params['timeout'], self.default_params['retry'], self.default_params['mandatory']))
             else:
-                default = False
+                self.mv_args[config['config_name']] = ""
+                if config['config_name'].split("_fls_config")[0] == self.mv_config_default:
+                    default = True
 
-            self.add_configuration(config['config_name'], config['brief'], config['description'], config['groupsState'], config['name'], config['parameters'], config['startState'], default)
+            self.add_configuration(config, default)
 
     def files(self):
         return self.flist
