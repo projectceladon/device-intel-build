3ec902a1b2ff0c4af6360fd707f38e1dbdd577dc
Dave Lin <dave.lin@intel.com>
2015-10-20 14:07:49 +0800

    Changed to support for sofia multiple firmware builds. For sofia, the make variable MV_CONFIG_DEFAULT_TYPE now must be specified with a period and the variant string appended (e.g., MV_CONFIG_DEFAULT_TYPE.s3gr10m6s).
    Change-Id: I543d290c23e4f96218784c55627c859f2e4cd57e
    Tracked-On: https://jira01.devtools.intel.com/browse/OAM-4058
    Signed-off-by: Dave Lin <dave.lin@intel.com>
    Reviewed-on: https://android.intel.com:443/425944

--- tasks/flashfiles.mk
+++ tasks/flashfiles.mk
@@ -25,7 +25,9 @@ ifneq ($(FLASHFILE_VARIANTS),)
 	  $(hide) mkdir -p $(dir $@)
 	  $(eval y = $(subst -, ,$(basename $(@F))))
 	  $(eval DEV = $(word 3, $(y)))
-	  $(hide) $(fftf) --variant=$(DEV) --mv_config_default=$(notdir $(MV_CONFIG_DEFAULT_TYPE)) $(BUILT_TARGET_FILES_PACKAGE) $@
+	  $(eval mvcfg_dev = $(MV_CONFIG_DEFAULT_TYPE.$(DEV)))
+	  $(if $(mvcfg_dev), $(eval mvcfg_default_arg = $(mvcfg_dev)),$(eval mvcfg_default_arg = $(MV_CONFIG_DEFAULT_TYPE)))
+	  $(hide) $(fftf) --variant=$(DEV) --mv_config_default=$(notdir $(mvcfg_default_arg)) $(BUILT_TARGET_FILES_PACKAGE) $@
   endif
 
   # Generate OTA fixup files
@@ -56,9 +58,15 @@ endif
 
 INTEL_FACTORY_FLASHFILES_TARGET := $(PRODUCT_OUT)/$(name).zip
 
+ifneq ($(SOFIA_FIRMWARE_VARIANTS),)
+mvcfg_default_arg = $(MV_CONFIG_DEFAULT_TYPE.$(firstword $(SOFIA_FIRMWARE_VARIANTS)))
+else
+mvcfg_default_arg = $(MV_CONFIG_DEFAULT_TYPE)
+endif
+
 $(INTEL_FACTORY_FLASHFILES_TARGET): $(BUILT_TARGET_FILES_PACKAGE) $(fftf) $(MKDOSFS) $(MCOPY)
 	$(hide) mkdir -p $(dir $@)
-	$(fftf) $(FLASHFILES_ADD_ARGS) --mv_config_default=$(notdir $(MV_CONFIG_DEFAULT_TYPE)) $(BUILT_TARGET_FILES_PACKAGE) $@
+	$(fftf) $(FLASHFILES_ADD_ARGS) --mv_config_default=$(notdir $(mvcfg_default_arg)) $(BUILT_TARGET_FILES_PACKAGE) $@
 
 ifeq ($(FLASHFILE_VARIANTS),)
 # Fast flashfiles is for engineering purpose only
@@ -80,7 +88,7 @@ FAST_FLASHFILES_DEPS := \
 fast_flashfiles: $(fftf) $(MKDOSFS) $(MCOPY) $(FAST_FLASHFILES_DEPS) | $(ACP)
 	$(hide) rm -rf $(FAST_FLASHFILES_DIR)
 	$(hide) mkdir -p $(FAST_FLASHFILES_DIR)
-	$(fftf) --mv_config_default=$(notdir $(MV_CONFIG_DEFAULT_TYPE)) --fast $(PRODUCT_OUT) $(FAST_FLASHFILES_DIR)
+	$(fftf) --mv_config_default=$(notdir $(mvcfg_default_arg)) --fast $(PRODUCT_OUT) $(FAST_FLASHFILES_DIR)
 
 # add dependencies
 droid: fast_flashfiles
