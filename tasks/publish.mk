e58398a6488d723b8da91df570ec5b3d57e9402e
yanqinlx <yanqinx.liu@intel.com>
2018-04-04 18:09:56 +0800

    [ACRN] remove target publish_acrnimage
    Change-Id: Id5d5421cca99a059f50ae0509cdcfaee06c14ee3
    Tracked-On:https://jira01.devtools.intel.com/browse/OAM-60886
    Signed-off-by: yanqinlx <yanqinx.liu@intel.com>
    Reviewed-on: https://android.intel.com:443/624737

--- tasks/publish.mk
+++ tasks/publish.mk
@@ -148,18 +148,6 @@ publish_gptimage:
 	@echo "Warning: Unable to fulfill publish_gptimage makefile request"
 endif
 
-.PHONY: publish_acrnimage
-ifdef ACRN_FLASHFILES
-publish_acrnimage: publish_mkdir_dest $(ACRN_FLASHFILES)
-	@$(ACP) $(ACRN_FLASHFILES) $(publish_dest)
-	$(hide) rm -rf $(publish_dest)/$(TARGET_PRODUCT).img
-	$(hide) rm -rf $(publish_dest)/$(TARGET_PRODUCT).craff
-else
-publish_acrnimage:
-	@echo "Warning: Unable to fulfill publish_acrnimage makefile request"
-endif
-
-
 .PHONY: publish_androidia_image
 ifdef ANDROID_IA_IMAGE
 publish_androidia_image: publish_mkdir_dest $(ANDROID_IA_IMAGE)
@@ -195,7 +183,7 @@ endif # PUBLISH_CONF
 
 PUBLISH_CI_FILES := $(DIST_DIR)/fastboot $(DIST_DIR)/adb
 .PHONY: publish_ci
-publish_ci: publish_liveimage publish_ota_flashfile publish_gptimage publish_acrnimage publish_ifwi publish_firmware_symbols $(PUB_OSAGNOSTIC_TAG) $(PUB_CMCC_ZIP) $(PLATFORM_RMA_TOOLS_ZIP)
+publish_ci: publish_liveimage publish_ota_flashfile publish_gptimage publish_ifwi publish_firmware_symbols $(PUB_OSAGNOSTIC_TAG) $(PUB_CMCC_ZIP) $(PLATFORM_RMA_TOOLS_ZIP)
 	$(if $(wildcard $(publish_dest)), \
 	  $(foreach f,$(PUBLISH_CI_FILES), \
 	    $(if $(wildcard $(f)),$(ACP) $(f) $(publish_dest);,)),)
@@ -235,5 +223,5 @@ PUBLISH_GOALS := $(DEFAULT_GOAL)
 endif
 
 .PHONY: publish
-publish: publish_mkdir_dest $(PUBLISH_GOALS) publish_ifwi publish_gptimage publish_acrnimage publish_firmware_symbols $(PUB_OSAGNOSTIC_TAG) publish_kf4abl_symbols $(PUB_CMCC_ZIP) publish_androidia_image
+publish: publish_mkdir_dest $(PUBLISH_GOALS) publish_ifwi publish_gptimage publish_firmware_symbols $(PUB_OSAGNOSTIC_TAG) publish_kf4abl_symbols $(PUB_CMCC_ZIP) publish_androidia_image
 	@$(ACP) $(DIST_DIR)/* $(publish_dest)
