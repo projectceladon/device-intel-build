b3ba717bc867fa8d0af8321bfc9a61cdbde5c0f9
Chen, ZhiminX <zhiminx.chen@intel.com>
2018-05-04 15:51:06 +0800

    Optimize the integration logic of ACPI config files
    The first-stage-mount-cfg files are merged into one in mixins.
    So detect avb or slot-ab is no need in make script.
    Change ABL vars output directory to PRODUCT_OUT.
    
    Change-Id: Iff728943e18c1d89d43ab18af94e009a372f70d1
    Tracked-On: OAM-61052
    Signed-off-by: Chen, ZhiminX <zhiminx.chen@intel.com>
    Reviewed-on: 625589

--- tasks/2ndbootloader.mk
+++ tasks/2ndbootloader.mk
@@ -68,9 +68,9 @@ $(INSTALLED_2NDBOOTLOADER_TARGET): $(BOARD_ABL_VARS)
 .PHONY: $(BOARD_ABL_VARS)
 $(BOARD_ABL_VARS):
 	$(hide) if [ -n "$(BOARD_ABL_ACPI_SRCS)" ];then \
-				cat $(BOARD_ABL_ACPI_FILE_PATHS) > $(ABL_BUILD_OUT)/acpi.tables; \
-				$(INTEL_PATH_VENDOR)/abl/abl_build_tools/ias_image_app -o $@ -i 0x30000 -v $(ABL_BUILD_OUT)/acpi.tables; \
-				rm -f $(ABL_BUILD_OUT)/acpi.tables;\
+				cat $(BOARD_ABL_ACPI_FILE_PATHS) > $(PRODUCT_OUT)/acpi.tables; \
+				$(INTEL_PATH_VENDOR)/abl/abl_build_tools/ias_image_app -o $@ -i 0x30000 -v $(PRODUCT_OUT)/acpi.tables; \
+				rm -f $(PRODUCT_OUT)/acpi.tables;\
 			fi
 
 endif
