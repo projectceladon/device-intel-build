322caac9f0fd986752f6570dc85b757e0f2514e1
Chen, ZhiminX <zhiminx.chen@intel.com>
2019-01-31 22:52:41 +0800

    Add variant support to USB disk image creation
    Change-Id: Ibccb00b04fbc125ee2ef9b125c64645ebe8b458b
    Tracked-On: OAM-76868
    Signed-off-by: Chen, ZhiminX <zhiminx.chen@intel.com>
    Reviewed-on: 659331
    Reviewed-on: 662720

--- tasks/create_usb_disk_image.mk
+++ tasks/create_usb_disk_image.mk
@@ -1,16 +1,43 @@
-ifdef BOARD_USB_DISK_IMAGE
+ifeq ($(BOARD_HAS_USB_DISK),true)
 
+ifeq ($(FLASHFILE_VARIANTS),)
 # 0x7e0000 is multiple of sectors/track(32 or 63), avoid mtools check error.
-$(BOARD_USB_DISK_IMAGE): fast_flashfiles $(MKDOSFS) $(MCOPY)
+$(BOARD_USB_DISK_IMAGES): fast_flashfiles $(MKDOSFS) $(MCOPY)
 	$(hide) rm -f $@ && $(MKDOSFS) -n UDISK -C $@ 0x7e0000
 	$(hide) $(MCOPY) -Q -i $@ $(FAST_FLASHFILES_DIR)/* ::
+else
+$(BOARD_USB_DISK_IMAGES): fast_flashfiles $(MKDOSFS) $(MCOPY)
+	for variant in $(FLASHFILE_VARIANTS); do \
+		eval "usb_disk$${variant}_files=(`ls $(FAST_FLASHFILES_DIR)/*_$${variant}*`)"; \
+	done; \
+	usb_disk_files=(`ls $(FAST_FLASHFILES_DIR)/*`); \
+	usb_disk_common_files=(); \
+	for file in $${usb_disk_files[@]}; do \
+		match=false; \
+		for variant in $(FLASHFILE_VARIANTS); do \
+			name=usb_disk$${variant}_files[*]; \
+			grep -q " $${file} " <<< " $${!name} " && match=true && break; \
+		done; \
+		$${match} && continue; \
+		usb_disk_common_files+=($${file}); \
+	done; \
+	for variant in $(FLASHFILE_VARIANTS); do \
+		name=usb_disk$${variant}_files[@]; \
+		rm -f $(BOARD_USB_DISK_IMAGE_PFX)-$${variant}.img; \
+		$(MKDOSFS) -n UDISK -C $(BOARD_USB_DISK_IMAGE_PFX)-$${variant}.img 0x7e0000; \
+		$(MCOPY) -Q -i $(BOARD_USB_DISK_IMAGE_PFX)-$${variant}.img $(FAST_FLASHFILES_DIR)/installer_$${variant}.cmd ::installer.cmd; \
+		$(MCOPY) -Q -i $(BOARD_USB_DISK_IMAGE_PFX)-$${variant}.img $${usb_disk_common_files[@]} $${!name} ::; \
+	done;
+endif
 
-flashfiles: $(BOARD_USB_DISK_IMAGE)
+flashfiles: $(BOARD_USB_DISK_IMAGES)
 
-publish_usb_disk_image: $(BOARD_USB_DISK_IMAGE) publish_mkdir_dest
-	$(hide) $(INTEL_PATH_BUILD)/createcraffimage.py --image $<
-	$(hide) $(ACP) $(<:.img=.craff) $(publish_dest)
+publish_usb_disk_image: $(BOARD_USB_DISK_IMAGES) publish_mkdir_dest
+	for image in $(BOARD_USB_DISK_IMAGES); do \
+		$(INTEL_PATH_BUILD)/createcraffimage.py --image $${image}; \
+		$(ACP) $${image/.img/.craff} $(publish_dest); \
+	done
 
 publish_ci: publish_usb_disk_image
 
-endif # BOARD_USB_DISK_IMAGE
+endif # BOARD_USB_DISK_IMAGES
