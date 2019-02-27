86c8a697b20daadf4cebf0a0d8a50e48edd0a409
Chen, ZhiminX <zhiminx.chen@intel.com>
2019-02-27 13:35:06 +0800

    Combine USB disk image targets.
    Combine USB disk image targets to avoid rebuilding USB disk
    image, and avoid multiple jobs building conflict on creating
    USB disk image.
    
    Change-Id: I0ed0c0d903ae302b442076bb19fb95e8691bf2a3
    Tracked-On: OAM-76868
    Signed-off-by: Chen, ZhiminX <zhiminx.chen@intel.com>
    Reviewed-on: 662460
    Reviewed-on: 662721

--- tasks/create_usb_disk_image.mk
+++ tasks/create_usb_disk_image.mk
@@ -1,12 +1,14 @@
 ifeq ($(BOARD_HAS_USB_DISK),true)
 
+.PHONY: usb_disk_images
+
 ifeq ($(FLASHFILE_VARIANTS),)
 # 0x7e0000 is multiple of sectors/track(32 or 63), avoid mtools check error.
-$(BOARD_USB_DISK_IMAGES): fast_flashfiles $(MKDOSFS) $(MCOPY)
-	$(hide) rm -f $@ && $(MKDOSFS) -n UDISK -C $@ 0x7e0000
-	$(hide) $(MCOPY) -Q -i $@ $(FAST_FLASHFILES_DIR)/* ::
+usb_disk_images: fast_flashfiles | $(MKDOSFS) $(MCOPY)
+	$(hide) rm -f $(BOARD_USB_DISK_IMAGES) && $(MKDOSFS) -n UDISK -C $(BOARD_USB_DISK_IMAGES) 0x7e0000
+	$(hide) $(MCOPY) -Q -i $(BOARD_USB_DISK_IMAGES) $(FAST_FLASHFILES_DIR)/* ::
 else
-$(BOARD_USB_DISK_IMAGES): fast_flashfiles $(MKDOSFS) $(MCOPY)
+usb_disk_images: fast_flashfiles | $(MKDOSFS) $(MCOPY)
 	for variant in $(FLASHFILE_VARIANTS); do \
 		eval "usb_disk$${variant}_files=(`ls $(FAST_FLASHFILES_DIR)/*_$${variant}*`)"; \
 	done; \
@@ -30,9 +32,7 @@ $(BOARD_USB_DISK_IMAGES): fast_flashfiles $(MKDOSFS) $(MCOPY)
 	done;
 endif
 
-flashfiles: $(BOARD_USB_DISK_IMAGES)
-
-publish_usb_disk_image: $(BOARD_USB_DISK_IMAGES) publish_mkdir_dest
+publish_usb_disk_image: usb_disk_images | publish_mkdir_dest
 	for image in $(BOARD_USB_DISK_IMAGES); do \
 		$(INTEL_PATH_BUILD)/createcraffimage.py --image $${image}; \
 		$(ACP) $${image/.img/.craff} $(publish_dest); \
