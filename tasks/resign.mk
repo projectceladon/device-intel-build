b351107eedf3816e6b824705f09172ea9aab0b82
Regnier, Philippe <philippe.regnier@intel.com>
2019-09-27 19:08:27 +0800

    ota: add build target publish_resign
    When called through build command, the new build target
    publish_resign allows build system to automatically resign
    current binaries and copy them in publish directory.
    In case of failure of resign operation, other build
    operations are not blocked and will perform normally.
    
    Tracked-On: OAM-86894
    Signed-off-by: phireg <philippe.regnier@intel.com>

---
+++ tasks/resign.mk
@@ -0,0 +1,12 @@
+otp := $(INTEL_PATH_BUILD)/test/ota-test-prepare
+
+.PHONY: publish_resign
+publish_resign: dist_files
+	export ANDROID_BUILD_TOP=$(PWD); $(otp) -o -s -t $(BUILT_TARGET_FILES_PACKAGE) resigned \
+	  && echo "Resign succeed" || { echo "Resign failed"; exit 0; }
+	@$(ACP) ota/$(TARGET_PRODUCT)/tfp-resigned.zip $(publish_dest) \
+	  && echo "tfp-resigned.zip copy succeed" || echo "tfp-resigned.zip copy failed"
+	@$(ACP) ota/$(TARGET_PRODUCT)/flashfiles-resigned.zip $(publish_dest) \
+	  && echo "flashfiles-resigned.zip copy succeed" || echo "flashfiles-resigned.zip copy failed"
+	@$(ACP) ota/$(TARGET_PRODUCT)/ota-resigned.zip $(publish_dest) \
+	  && echo "ota-resigned.zip copy succeed" || echo "ota-resigned.zip copy failed"
