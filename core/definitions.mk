ff4b2830c22b562d7ab5ef6a976f70bedb463a1b
JianFeng,Zhou <jianfeng.zhou@intel.com>
2019-11-15 14:17:52 +0800

    disable execstack for kernelflinger
    Add flag: -Wa,--noexecstack
    
    Change-Id: I6cd2c588a631c60036516180aae12b7f87486b46
    Tracked-On: OAM-88872
    Signed-off-by: JianFeng,Zhou <jianfeng.zhou@intel.com>

--- core/definitions.mk
+++ core/definitions.mk
@@ -75,7 +75,8 @@ TARGET_IAFW_GLOBAL_CFLAGS := -ggdb -O3 -fstack-protector-strong \
 	-Wno-unused-function \
 	-Wno-tautological-pointer-compare \
 	-Wformat -Wformat-security \
-	-D_FORTIFY_SOURCE=2
+	-D_FORTIFY_SOURCE=2 \
+	-Wa,--noexecstack
 
 TARGET_IAFW_GLOBAL_LDFLAGS := -nostdlib --no-undefined \
 	--fatal-warnings -Bsymbolic -znocombreloc -znoexecstack -zrelro -znow
